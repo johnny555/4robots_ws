1dceb5bbb3694e23eadd613993fed63e9733ca6f
John Vial <badwolf.johnnyv@gmail.com>
2024-03-10 08:08:14 +0000

    updating setup script

--- .vscode/scripts/setup.sh
+++ .vscode/scripts/setup.sh
@@ -6,7 +6,8 @@ if [ ! -f "apps/FreeCAD.AppImage" ]; then
     wget -O apps/FreeCAD.AppImage https://github.com/FreeCAD/FreeCAD/releases/download/0.21.2/FreeCAD-0.21.2-Linux-x86_64.AppImage
 fi
 
-vcs import < src/workspace.repos src
+vcs import src < src/workspace.repos
+vcs pull src
 sudo apt-get update
 rosdep update --rosdistro=humble
 rosdep install --from-paths src --ignore-src -y --rosdistro=humble
