467db8d32a58c9712049075303d8d80a506cffdc
John Vial <badwolf.johnnyv@gmail.com>
2024-03-15 07:29:52 +0000

    updating setup so it doesn't break when folder already populated

--- .vscode/scripts/setup.sh
+++ .vscode/scripts/setup.sh
@@ -5,8 +5,10 @@ if [ ! -f "apps/FreeCAD.AppImage" ]; then
     echo " ============ Downloading FreeCAD ===================="
     wget -O apps/FreeCAD.AppImage https://github.com/FreeCAD/FreeCAD/releases/download/0.21.2/FreeCAD-0.21.2-Linux-x86_64.AppImage
 fi
-
-vcs import src < src/workspace.repos
+# Checks if krytn directory exist. If so, we won't try to import all the repos
+if [ ! -d "src/krytn"]; then
+    vcs import src < src/workspace.repos
+fi
 vcs pull src
 sudo apt-get update
 rosdep update --rosdistro=humble
