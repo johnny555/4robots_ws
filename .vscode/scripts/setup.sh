8f71456434878eac4c58b836c464da0b9b6d6b36
John Vial <badwolf.johnnyv@gmail.com>
2024-03-25 05:20:28 +0000

    fix bug in setup.sh

--- .vscode/scripts/setup.sh
+++ .vscode/scripts/setup.sh
@@ -6,7 +6,7 @@ if [ ! -f "apps/FreeCAD.AppImage" ]; then
     wget -O apps/FreeCAD.AppImage https://github.com/FreeCAD/FreeCAD/releases/download/0.21.2/FreeCAD-0.21.2-Linux-x86_64.AppImage
 fi
 # Checks if krytn directory exist. If so, we won't try to import all the repos
-if [ ! -d "src/krytn"]; then
+if [ ! -d "src/krytn" ]; then
     vcs import src < src/workspace.repos
 fi
 vcs pull src
