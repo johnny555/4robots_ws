93691d0f5cce8b62ef6526b9c06e5d097af9e039
John Vial <badwolf.johnnyv@gmail.com>
2024-07-13 10:24:32 +0000

    Setting up things

--- .devcontainer/setup_container.bash
+++ .devcontainer/setup_container.bash
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-source /opt/ros/humble/local_setup.bash
+source /opt/ros/jazzy/local_setup.bash
 
 # Create user ros, and allow it to install stuff. 
 adduser --disabled-password --gecos "docker user" ros
 echo 'ros ALL=(ALL) NOPASSWD:ALL' > /etc/sudoers.d/ros && chmod 0440 /etc/sudoers.d/ros
 chown -R ros /workspace
 
+# Get FreeCAD 
+sudo --user=ros wget -O /home/ros/FreeCAD.AppImage https://github.com/FreeCAD/FreeCAD-Bundle/releases/download/weekly-builds/FreeCAD_weekly-builds-38079-conda-Linux-x86_64-py311.AppImage
+sudo chmod +x /home/ros/FreeCAD.AppImage 
 
 # Setup FreeCAD Cross For FreeCAD
-
 sudo --user=ros mkdir -p /home/ros/.local/share/FreeCAD/Mod/
 sudo --user=ros git clone https://github.com/galou/freecad.cross.git /home/ros/.local/share/FreeCAD/Mod/freecad.cross
 
@@ -20,11 +22,12 @@ sudo --user=ros pip install black urdf-parser-py
 
 
 # Make it so that sourcing happens automatically
-echo "source /opt/ros/humble/setup.bash" >> /home/ros/.bashrc
+echo "source /opt/ros/jazzy/setup.bash" >> /home/ros/.bashrc
 echo "source /workspace/install/setup.bash" >> /home/ros/.bashrc
 
 # Suppress deprecated setuptools warning
 echo "PYTHONWARNINGS=\"ignore:setup.py install is deprecated::setuptools.command.install,ignore:easy_install command is deprecated::setuptools.command.easy_install\"; export PYTHONWARNINGS" >> /home/ros/.bashrc
 
-# Add GAZEBO path so we can easily include models
-echo "export IGN_GAZEBO_RESOURCE_PATH=/workspace/install/krytn/share/krytn/models/:/opt/ros/humble/share/realsense2_description/meshes/" >> /home/ros/.bashrc
+# Add GAZEBO path so we can easily include models. Sholud be able to use package:// syntax now. 
+echo "export GZ_SIM_RESOURCE_PATH=/workspace/install/krytn/:/opt/ros/jazzy/share/" >> /home/ros/.bashrc
+echo "export SDF_PATH=/workspace/install/krytn/:/opt/ros/jazzy/share/" >> /home/ros/.bashrc
--- .vscode/scripts/run_freecad.sh
+++ .vscode/scripts/run_freecad.sh
@@ -3,11 +3,6 @@
 source /opt/ros/humble/setup.bash
 source install/setup.bash
 
-if [ ! -f "apps/FreeCAD.AppImage" ]; then
-    echo " ============ Downloading FreeCAD ===================="
-    wget -O apps/FreeCAD.AppImage https://github.com/FreeCAD/FreeCAD/releases/download/0.21.2/FreeCAD-0.21.2-Linux-x86_64.AppImage
 
-fi
-chmod +x ./apps/FreeCAD.AppImage
 echo "Starting FreeCAD, please wait ..."
-./apps/FreeCAD.AppImage --appimage-extract-and-run
+/home/ros/FreeCAD.AppImage --appimage-extract-and-run
